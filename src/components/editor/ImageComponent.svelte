fa0ba1f5e498efb198235ad070525a405cebf20c
Lucas <somfic@gmail.com>
2022-08-22 23:48:42 +0200

    Upgraded ImageComponent

--- src/components/editor/ImageComponent.svelte
+++ src/components/editor/ImageComponent.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+    let edit = true;
     export let value: string;
+    export let componentManagement;
 </script>
 
-<img src={value} />
+{#if edit}
+    <input
+        autofocus={componentManagement.isNewest()}
+        type="text"
+        placeholder="Image source"
+        bind:value
+    />
+    <button
+        on:click={() => {
+            edit = false;
+        }}>Apply</button
+    >
+{:else}
+    <img src={value} alt="" />
+{/if}
