27f69bc71b8a1a61d0558cffa5abaa9a1e8fd199
Lucas <somfic@gmail.com>
2022-08-23 00:43:29 +0200

    Added drag and drop

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-import { ComponentManagement } from "./common/commands";
+    import { ComponentManagement } from "./common/commands";
+    import { dndzone } from "svelte-dnd-action";
 
-    import { document } from "./components";
+    import { document, lastId } from "./components";
     import GenericComponent from "./components/editor/GenericComponent.svelte";
+    import H1Component from "./components/editor/H1Component.svelte";
 
     let hoverIndex = -1;
     let isHovering = false;
@@ -24,11 +26,19 @@ import { ComponentManagement } from "./common/commands";
     function reset() {
         document.set([
             {
+                id: 1,
+                component: H1Component,
+                value: "Test 1",
+                createdAt: Date.now(),
+            },
+            {
+                id: 2,
                 component: GenericComponent,
                 value: "",
                 createdAt: Date.now(),
             },
         ]);
+        $lastId = 2;
     }
 
     $: newestComponentIndex = $document.indexOf(
@@ -37,31 +47,56 @@ import { ComponentManagement } from "./common/commands";
         )
     );
 
-
-
     export let items;
+
+    function backspaceManager(e) {
+        const index = e.target.parentElement.getAttribute("data-index");
+        console.log(index);
+    }
+
+    function handleDndConsider(e) {
+        console.log("considering", e);
+        $document = e.detail.items;
+    }
+
+    function handleDndFinalize(e) {
+        console.log("finalizing", e);
+        $document = e.detail.items;
+    }
 </script>
 
 <button on:click={() => reset()}>clear</button>
 <p>hoverindex: {hoverIndex}</p>
 <p>latestindex: {newestComponentIndex}</p>
-<div class="items" bind:this={items}>
-    {#each $document as item, index}
+
+<div
+    class="items"
+    use:dndzone={{ items: $document }}
+    on:consider={handleDndConsider}
+    on:finalize={handleDndFinalize}
+    bind:this={items}
+    on:keydown={backspaceManager}
+>
+    {#each $document as item, index (item.id)}
         <div
+            data-index={index}
             class={"item " + (hoverIndex === index ? "hover" : "")}
             draggable="true"
             on:dragstart={(e) => dragStart(e, index)}
             on:drop|preventDefault={(e) => drop(e, index)}
             on:dragenter={(e) => dragEnter(e, index)}
         >
-            <svelte:component
-                this={item.component}
-                bind:value={item.value}
-                self={new ComponentManagement(
-                    index,
-                    newestComponentIndex === index
-                )}
-            />
+            <div class="item-content">
+                <svelte:component
+                    this={item.component}
+                    class="content"
+                    bind:value={item.value}
+                    self={new ComponentManagement(
+                        index,
+                        newestComponentIndex === index
+                    )}
+                />
+            </div>
         </div>
     {/each}
 </div>
@@ -73,8 +108,15 @@ import { ComponentManagement } from "./common/commands";
         align-items: flex-start;
 
         .item {
+            padding-left: 50px;
+
             &.hover {
                 background-color: #eee;
+            }
+
+            .item-content {
+                z-index: 10;
+                cursor: text;
             }
         }
     }
