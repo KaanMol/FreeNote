c1387109461c871f96f61c268052165728e3f3e6
Lucas <somfic@gmail.com>
2022-08-24 16:27:55 +0200

    Save images in Base64
    Co-Authored-By: Kaan Mol <21340806+KaanMol@users.noreply.github.com>

--- src/components/editor/ImageComponent.svelte
+++ src/components/editor/ImageComponent.svelte
@@ -2,6 +2,15 @@
     export let value: string;
     export let self;
     let edit = value === "" || value === undefined;
+    function uploadLocal(element) {
+        let file = element.target.files[0];
+        let reader = new FileReader();
+        reader.onloadend = function () {
+            value = reader.result as string;
+            edit = false;
+        };
+        reader.readAsDataURL(file);
+    }
 </script>
 
 {#if edit}
@@ -15,6 +24,8 @@
         on:click={() => {
             edit = false;
         }}>Apply</button
+    >
+    <input type="file" on:change={uploadLocal} />
     >
 {:else}
     <img src={value} alt="" />
