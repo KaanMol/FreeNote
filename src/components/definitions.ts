591b7ea64435ba2c14a2cf5147cfeb266c7ffdb0
Lucas <29230467+Somfic@users.noreply.github.com>
2022-08-28 01:59:29 +0200

    Manage component definitions

---
+++ src/components/definitions.ts
@@ -0,0 +1,11 @@
+import type { ComponentDefinition } from "./components";
+
+const definitions: ComponentDefinition<any, any>[] = [];
+
+export function registerComponent<T extends ComponentDefinition<any, any>>(definition: T) {
+    definitions.push(definition);
+}
+
+export function getComponent(identifier: string): ComponentDefinition<any, any> | undefined {
+    return definitions.find((definition: ComponentDefinition<any, any>) => definition.identifier === identifier);
+}
