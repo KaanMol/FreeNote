29d58fc00dd295f7ccd62a9fb099188ac91735df
Lucas <29230467+Somfic@users.noreply.github.com>
2022-08-28 01:59:56 +0200

    Created basic H1 component

---
+++ src/plugins/standard/H1Component.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export class Test {}
+
+    export let state: string;
+</script>
+
+<h1 contenteditable="true" bind:textContent={state}>{state}</h1>
---
+++ src/plugins/standard/H1Component.ts
@@ -0,0 +1,19 @@
+import { ComponentDefinition, ComponentInformation, ShorthandDefinition } from "../../components/components";
+import H1Component from "./H1Component.svelte";
+
+export class H1ComponentDefinition extends ComponentDefinition<H1Component, String> {
+    identifier = "freenote.h1";
+
+    componentType = H1Component;
+
+    information = new ComponentInformation("Heading 1", "Add a Heading 1 to your document");
+    shorthand = new ShorthandDefinition("h1", "#");
+
+    serialize(state: string): string {
+        return state;
+    }
+
+    deserialize(serialized: string): string {
+        return serialized;
+    }
+}
