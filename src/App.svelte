de981e66624e55b04025ad6f6d0614869e62c353
Lucas <29230467+Somfic@users.noreply.github.com>
2022-08-28 02:00:26 +0200

    Use new component api

--- src/App.svelte
+++ src/App.svelte
@@ -1,163 +1,30 @@
 <script lang="ts">
-    import { ComponentManagement } from "./common/commands";
-    import { dndzone } from "svelte-dnd-action";
+    import { registerComponent } from "./components/definitions";
+    import { clearDocument, insertInDocument, document, serializeDocument, deserializeDocument } from "./document";
+    import { CheckboxComponentDefinition } from "./plugins/standard/CheckboxComponent";
+    import { H1ComponentDefinition } from "./plugins/standard/H1Component";
 
-    import { document, lastId, serialize } from "./components";
-    import GenericComponent from "./components/editor/GenericComponent.svelte";
-    import H1Component from "./components/editor/H1Component.svelte";
+    clearDocument();
 
-    let hoverIndex = -1;
-    let newestComponentIndex = 0;
+    registerComponent(new H1ComponentDefinition());
+    registerComponent(new CheckboxComponentDefinition());
 
-    let board = [
-        {
-            id: 1,
-            name: "TODO",
-            items: [
-                { id: 41, name: "item41" },
-                { id: 42, name: "item42" },
-                { id: 43, name: "item43" },
-                { id: 44, name: "item44" },
-                { id: 45, name: "item45" },
-                { id: 46, name: "item46" },
-                { id: 47, name: "item47" },
-                { id: 48, name: "item48" },
-                { id: 49, name: "item49" },
-            ],
-        },
-        {
-            id: 2,
-            name: "DOING",
-            items: [],
-        },
-        {
-            id: 3,
-            name: "DONE",
-            items: [],
-        },
-    ];
-
-    function reset() {
-        document.set([
-            {
-                id: 1,
-                component: H1Component,
-                value: "Test 1",
-                createdAt: Date.now(),
-            },
-            {
-                id: 2,
-                component: GenericComponent,
-                value: "",
-                createdAt: Date.now(),
-            },
-        ]);
-        $lastId = 2;
-    }
-
-    $: newestComponentIndex = $document.indexOf(
-        $document.reduce((prev, current) =>
-            prev.createdAt > current.createdAt ? prev : current
-        )
-    );
-
-    export let items;
-
-    function backspaceManager(e) {
-        const index = e.target.parentElement.getAttribute("data-index");
-        console.log(index);
-    }
-
-    function handleDndConsider(e) {
-        console.log("considering", e);
-        $document = e.detail.items;
-    }
-
-    function handleDndFinalize(e) {
-        console.log("finalizing", e);
-        $document = e.detail.items;
-    }
+    insertInDocument(new H1ComponentDefinition(), "Hello world!");
 
     function save() {
-        console.log(serialize());
+        localStorage.setItem("save", serializeDocument());
+    }
+
+    function load() {
+        deserializeDocument(localStorage.getItem("save"));
     }
 </script>
 
-<button on:click={() => reset()}>Clear contents</button>
-<button on:click={() => save()}>Save contents</button>
-<div
-    class="items"
-    use:dndzone={{ items: $document }}
-    on:consider={handleDndConsider}
-    on:finalize={handleDndFinalize}
-    bind:this={items}
-    on:keydown={backspaceManager}
->
-    {#each $document as item, index (item.id)}
-        <div
-            data-index={index}
-            class={"item " + (hoverIndex === index ? "hover" : "")}
-            draggable="true"
-        >
-            <div class="item-content">
-                <span class="material-icons"> drag_indicator </span>
-                <div>
-                    <svelte:component
-                        this={item.component}
-                        class="content"
-                        bind:value={item.value}
-                        self={new ComponentManagement(
-                            index,
-                            newestComponentIndex === index
-                        )}
-                    />
-                </div>
-            </div>
-        </div>
+<button on:click={() => save()}>Save</button>
+<button on:click={() => load()}>Load</button>
+
+<div class="items">
+    {#each $document as item}
+        <svelte:component this={item.definition.componentType} bind:state={item.state} />
     {/each}
 </div>
-
-<style lang="scss">
-    button {
-        background: #ff7f77;
-        color: black;
-        font-weight: bold;
-        padding: 0.5rem 1rem;
-        border-radius: 0.25rem;
-        border: none;
-    }
-
-    .items {
-        display: grid;
-        grid-template-rows: auto;
-    }
-
-    .item {
-        &.hover {
-            background-color: #eee;
-        }
-
-        .item-content {
-            width: 100%;
-            display: grid;
-            grid-template-columns: 40px max-content;
-            align-items: center;
-            z-index: 10;
-            cursor: text;
-            &:hover {
-                & .material-icons {
-                    opacity: 1;
-                    transition: 250ms;
-                }
-            }
-
-            .material-icons {
-                // display: none;
-                opacity: 0;
-                cursor: grab;
-                margin-right: 15px;
-                transition: 250ms;
-            }
-        }
-    }
-</style>
