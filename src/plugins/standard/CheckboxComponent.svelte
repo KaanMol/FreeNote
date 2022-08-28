c058074e85d63558bf9aeadffce24ebd24cd4384
Lucas <29230467+Somfic@users.noreply.github.com>
2022-08-28 02:00:04 +0200

    Created basic checkbox component

---
+++ src/plugins/standard/CheckboxComponent.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let state: boolean;
+</script>
+
+<label for="test">Lol</label>
+<input id="test" type="checkbox" bind:checked={state} />
---
+++ src/plugins/standard/CheckboxComponent.ts
@@ -0,0 +1,19 @@
+import { ComponentDefinition, ComponentInformation, ShorthandDefinition } from "../../components/components";
+import CheckboxComponent from "./CheckboxComponent.svelte";
+
+export class CheckboxComponentDefinition extends ComponentDefinition<CheckboxComponent, Boolean> {
+    identifier = "freenote.checkbox";
+
+    componentType = CheckboxComponent;
+
+    information = new ComponentInformation("Checkbox", "Add a checkbox to your document");
+    shorthand = new ShorthandDefinition("checkbox", "c");
+
+    serialize(state: boolean): string {
+        return state.toString();
+    }
+
+    deserialize(serialized: string): boolean {
+        return serialized === "true";
+    }
+}
